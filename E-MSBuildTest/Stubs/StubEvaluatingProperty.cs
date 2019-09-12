632a166f305ad9e2b1b9ad779c24a42b6fa12606
Denis Kuzmin [ GitHub/3F ] <entry.reg@gmail.com>
2019-09-12 20:34:18 +0300

    Imported from 084635c250445bbe2aa67ce2f65108724e2e2d44
    https://github.com/3F/vsSolutionBuildEvent

---
+++ E-MSBuildTest/Stubs/StubEvaluatingProperty.cs
@@ -0,0 +1,18 @@
+﻿namespace EvMSBuildTest.Stubs
+{
+    internal class StubEvaluatingProperty: EvMSBuilderAcs
+    {
+        public override string GetPropValue(string name, string project)
+        {
+            if(UVars.IsExist(name, project)) {
+                return GetUVarValue(name, project);
+            }
+            return $"[P~{name}~{project}]";
+        }
+
+        protected override string Obtain(string unevaluated, string project)
+        {
+            return $"[E~{unevaluated}~{project}]";
+        }
+    }
+}
