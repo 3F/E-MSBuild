fbf7be4ef02c500cfb75f6da1629304d8cb458da
Denis Kuzmin [ GitHub/3F ] <x-3F@outlook.com>
2020-07-31 14:56:34 +0300

    Upgraded build to modern structure:
    * vsSolutionBuildEvent 1.14
    * Projects up to VS 16
    * Only Debug;Release;PublicRelease
    
    Also updated LSender to 1.15

--- build.bat
+++ build.bat
@@ -1,42 +1,22 @@
 @echo off
 
-REM # Version of used CI.MSBuild
-set cimdll=packages\vsSBE.CI.MSBuild\bin\CI.MSBuild.dll
-
-set _msbuild=tools\hMSBuild -notamd64
-
-REM # Verbosity level by default: q[uiet], m[inimal], n[ormal], d[etailed], and diag[nostic].
-set level=minimal
-
-set _gnt=tools\gnt
-
-:::: --------
+set cim=packages\vsSolutionBuildEvent\cim.cmd -vsw-priority Microsoft.NetCore.Component.SDK
+set _gnt=tools/gnt
 
 set reltype=%~1
-
-if "%reltype%"=="" (
-    set "reltype=DCI"
+if not defined reltype (
+    set "reltype=Release"
 )
 
-:::: --------
+call %_gnt% /p:wpath="%cd%" /p:ngconfig=".gnt/packages.config" /nologo /v:m /m:7 || goto err
+call %cim% E-MSBuild.sln /v:m /m:7 /p:Configuration="%reltype%" || goto err
 
-set __p_call=1
-
-:: Activate vsSBE
-
-call %_gnt% /p:ngpath="%cd%/packages" /p:ngconfig="%cd%/.gnt/packages.config" || goto err
-
-:: Build
-set bnode=%_msbuild% E-MSBuild.sln /m:4 /l:"%cimdll%" /p:Platform="Any CPU" /v:%level% /nologo
-
-call %bnode% /p:Configuration=%reltype%_SDK15 /t:Rebuild || goto err
-
-goto ok
+goto exit
 
 :err
 
 echo. Build failed. 1>&2
 exit /B 1
 
-:ok
+:exit
 exit /B 0
